69c45d09b098e4d0c116339060d73181ab70c10b
prachyo <prachyosarker565@gmail.com>
2022-08-19 20:35:28 -0400

    added terminal_clear()

--- src/tty.c
+++ src/tty.c
@@ -51,3 +51,7 @@ void terminal_write(const char* data, size_t size) {
 void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
 }
+
+void terminal_clear(void) {
+    return terminal_initialize();
+}
