a5ab239e773d5cdb690e9691ec4358e9e3426f2c
prachyo <prachyosarker565@gmail.com>
2022-08-20 22:27:35 -0400

    added support for scrolling and \n

--- src/tty.c
+++ src/tty.c
@@ -16,7 +16,7 @@
 static size_t terminal_row;
 static size_t terminal_column;
 static uint8_t terminal_color;
-static uint8_t *terminal_buffer = (uint8_t *) VGA_MEMORY;
+static uint16_t *terminal_buffer = (uint8_t *) VGA_MEMORY;
 
 void tty_init(void) {
 	terminal_row = 0;
@@ -29,25 +29,58 @@ void tty_init(void) {
 			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
+	tty_mv_cursor(0,0);
 }
 
 void tty_setcolor(uint8_t color) {
 	terminal_color = color;
 }
 
+void tty_mv_cursor_up() {
+	terminal_row--;
+	tty_mv_cursor(terminal_row, terminal_column);
+}
+
+void tty_mv_cursor_down() {
+	terminal_row++;
+	tty_mv_cursor(terminal_row, terminal_column);
+}
+
+void tty_mv_cursor_start() {
+	terminal_column = 0;
+	tty_mv_cursor(terminal_row, terminal_column);
+}
+
+void tty_mv_cursor_end() {
+	terminal_column = 79;
+	tty_mv_cursor(terminal_row, terminal_column);
+}
+
+char tty_readc(size_t row, size_t col) {
+	const size_t index = row * VGA_WIDTH + col;
+	char *cell = terminal_buffer[index];
+	return cell;
+
+}
+
 void tty_put_at(unsigned char c, uint8_t color, size_t x, size_t y) {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
+	tty_advance_cursor();
 }
 
 void tty_putc(char c) {
 	unsigned char uc = c;
-	tty_put_at(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+	if(c != '\n')
+		tty_put_at(uc, terminal_color, terminal_column, terminal_row);
+
+	if(c == '\n') {
+		tty_mv_cursor_down();
+		tty_mv_cursor_start();
+	} else {
+		tty_advance_cursor();
 	}
+	
 }
 
 void tty_write(const char* data, size_t size) {
@@ -65,11 +98,40 @@ void tty_clear(void) {
     return tty_init();
 }
 
-void tty_mv_cursor(uint16_t row, uint16_t col)
-{
-    uint16_t loc = row*VGA_WIDTH + col;
-    outb(TTY_CURSOR_INDEX_PORT, TTY_HIGH_BYTE);
-    outb(TTY_CURSOR_DATA_PORT, loc >> 8);
+void tty_set_cursor(uint16_t dest) {
+	outb(TTY_CURSOR_INDEX_PORT, TTY_HIGH_BYTE);
+    outb(TTY_CURSOR_DATA_PORT, dest >> 8);
     outb(TTY_CURSOR_INDEX_PORT, TTY_LOW_BYTE);
-    outb(TTY_CURSOR_DATA_PORT, loc);
+    outb(TTY_CURSOR_DATA_PORT, dest);
+}
+
+void tty_mv_cursor(uint16_t row, uint16_t col) {
+    uint16_t loc = row*VGA_WIDTH + col;
+	terminal_row = row;
+	terminal_column = col;
+	tty_set_cursor(loc);
+}
+
+void tty_advance_cursor(void) {
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			tty_scroll();
+	}
+}
+
+static void tty_scroll()
+{
+    uint32_t r, c;
+    for (r = 1; r < VGA_HEIGHT; ++r) {
+        for (c = 0; c < VGA_WIDTH; ++c) {
+            tty_put_at(tty_readc(r, c), terminal_color, r - 1, c);
+        }
+    }
+
+    for (c = 0; c < VGA_WIDTH; ++c) {
+        fb_write(' ', VGA_HEIGHT - 1, c);
+    }
+
+	tty_mv_cursor(VGA_HEIGHT -1, 0);
 }
