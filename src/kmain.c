6e8c7b0f395e5725b8be7c6192808477801c467f
prachyo <prachyosarker565@gmail.com>
2022-08-21 02:24:39 -0400

    added basic gdt impl

--- src/kmain.c
+++ src/kmain.c
@@ -1,4 +1,5 @@
 #include "tty.h"
+#include "gdt.h"
 
 #define UNUSED_ARGUMENT(x) (void) x
 int kmain(void *mboot, unsigned int magic_number)
@@ -6,6 +7,7 @@ int kmain(void *mboot, unsigned int magic_number)
     UNUSED_ARGUMENT(mboot);
     UNUSED_ARGUMENT(magic_number);
     tty_init();
+    gdt_init();
     tty_puts("hello world!");
     tty_mv_cursor(0, 0);
     return 0xDEADBEEF;
